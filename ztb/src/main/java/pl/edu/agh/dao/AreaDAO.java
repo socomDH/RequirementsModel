6f90f767a76e2aefe43d0e426b515e4dbdd87522
piotrek <pzdzisiu@gmail.com>
2015-01-26 23:28:13 +0100

    createArea fixed

--- ztb/src/main/java/pl/edu/agh/dao/AreaDAO.java
+++ ztb/src/main/java/pl/edu/agh/dao/AreaDAO.java
@@ -3,6 +3,7 @@ package pl.edu.agh.dao;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import pl.edu.agh.model.IdResponse;
 
 public interface AreaDAO {
 //  @SqlUpdate("create table something (id int primary key, name varchar(100))")
@@ -11,6 +12,9 @@ public interface AreaDAO {
 //  @SqlUpdate("insert into something (id, name) values (:id, :name)")
 //  void insert(@Bind("id") int id, @Bind("name") String name);
 
-  @SqlUpdate("insert into areas (name) values (:name)")
-  long createArea(@Bind("name") String name);
+  @SqlUpdate("INSERT INTO areas (name) VALUES (:name)")
+  void createArea(@Bind("name") String name);
+  
+  @SqlQuery("SELECT id FROM areas where name = :name")
+  long findAreaIdByName(@Bind("name") String name);
 }
